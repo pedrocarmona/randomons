f7f3bb14e094a2341764c4299d1842733a76c5cf
pjpires <pjpires10@gmail.com>
2013-05-13 12:25:23 +0100

    finished ranking without asynctask

---
+++ src/com/example/activities/ChooseRandomon.java
@@ -0,0 +1,23 @@
+package com.example.activities;
+
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class ChooseRandomon extends SherlockActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.choose_randomon);
+
+        final ActionBar bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setTitle("Ranking");
+
+    }
+}
